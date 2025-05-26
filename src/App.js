1f1bd78ce0846a5ee45af0a86921cc7c6c4925b4
PaiigeTech <156714524+PaiigeTech@users.noreply.github.com>
2025-05-26 23:54:08 +0200

    Added footer.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ export default function App() {
     <h1>Weather App</h1>
     <Weather  defaultCity ="LONDON"/>
     <footer>
-      <p>This project was coded by <a href="github.com/paiige/" target= "_blank">Phindile Petunia</a> and is open sourced on Github and Netlify"</p>
+      <p>This project was coded by Phindile Petunia {" "} and is {" "}
+     <a href="https://github.com/PaiigeTech/weather-react-app.git" target="_blank">open sourced on Github</a> {" "}
+     and {" "}
+     <a href ="https://app.netlify.com/projects/weather-react-petunia">hosted on Netlify</a>
+      </p>
     </footer>
     </div>
    
